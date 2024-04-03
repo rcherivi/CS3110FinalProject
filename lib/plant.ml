c1e85ab72284b5d80d8b013d23a11cde5a34d152
Jeana Han <jjh358@cornell.edu>
2024-04-03 15:48:52 -0400

    Implemented water function jjh358 jjh358@cornell.edu
    - Added a hydration attribute for plants
    - Implemented water function

--- lib/plant.ml
+++ lib/plant.ml
@@ -1,4 +1,7 @@
-type plant_type = { height : int }
+type plant_type = {
+  height : int;
+  hydration : int;
+}
 
 type t =
   | Flower of plant_type
@@ -8,22 +11,37 @@ type t =
 
 let feed plant =
   match plant with
-  | Flower { height = x } -> Flower { height = x + 1 }
-  | Peach { height = x } -> Peach { height = x + 1 }
-  | Strawberry { height = x } -> Strawberry { height = x + 1 }
-  | Cactus { height = x } -> Cactus { height = x + 1 }
+  | Flower { height = x; hydration = h } ->
+      Flower { height = x + 1; hydration = h }
+  | Peach { height = x; hydration = h } ->
+      Peach { height = x + 1; hydration = h }
+  | Strawberry { height = x; hydration = h } ->
+      Strawberry { height = x + 1; hydration = h }
+  | Cactus { height = x; hydration = h } ->
+      Cactus { height = x + 1; hydration = h }
+
+let water plant =
+  match plant with
+  | Flower { height = x; hydration = h } ->
+      Flower { height = x; hydration = h + 1 }
+  | Peach { height = x; hydration = h } ->
+      Peach { height = x; hydration = h + 1 }
+  | Strawberry { height = x; hydration = h } ->
+      Strawberry { height = x; hydration = h + 1 }
+  | Cactus { height = x; hydration = h } ->
+      Cactus { height = x; hydration = h + 1 }
 
 let print_plant plant =
   match plant with
-  | Flower { height = h } -> if h < 3 then "🍃" else "🌹"
-  | Peach { height = h } -> if h < 5 then "🌱" else "🍊"
-  | Strawberry { height = h } -> if h < 2 then "🍂" else "🍓"
-  | Cactus { height = h } -> if h < 7 then "🌿" else "🌵"
+  | Flower { height = h; hydration = _ } -> if h < 3 then "🍃" else "🌹"
+  | Peach { height = h; hydration = _ } -> if h < 5 then "🌱" else "🍊"
+  | Strawberry { height = h; hydration = _ } -> if h < 2 then "🍂" else "🍓"
+  | Cactus { height = h; hydration = _ } -> if h < 7 then "🌿" else "🌵"
 
 let create_plant plant_name =
   match plant_name with
-  | "Flower" -> Flower { height = 3 }
-  | "Peach" -> Peach { height = 3 }
-  | "Strawberry" -> Strawberry { height = 4 }
-  | "Cactus" -> Cactus { height = 5 }
+  | "Flower" -> Flower { height = 3; hydration = 25 }
+  | "Peach" -> Peach { height = 3; hydration = 25 }
+  | "Strawberry" -> Strawberry { height = 4; hydration = 25 }
+  | "Cactus" -> Cactus { height = 5; hydration = 10 }
   | _ -> failwith "we don't have this plant yet..."
--- lib/plant.mli
+++ lib/plant.mli
@@ -1,5 +1,6 @@
 type t
 
 val feed : t -> t
+val water : t -> t
 val create_plant : string -> t
 val print_plant : t -> string
