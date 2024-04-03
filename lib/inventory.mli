83acd5a29737c52ea6701b6dcf61dd82578e3822
Joanna Andrews <jda242@cornell.edu>
2024-04-03 17:44:47 -0400

    Update inventory files
    updated sell function
    jda242
    jda242@cornell.edu

--- lib/inventory.mli
+++ lib/inventory.mli
@@ -2,5 +2,5 @@ type t
 
 val create_inventory : t
 val harvest : string -> t -> Garden.t -> t * Garden.t
-val sell : string -> t -> Garden.t -> t * Garden.t
+val sell : string -> int -> t -> Garden.t -> t * Garden.t
 val print : t -> unit
