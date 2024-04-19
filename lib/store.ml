8804b0378f4a9410aca7c654ac430fca4aa7d153
Mishita Kaja <mhk225@cornell.edu>
2024-04-19 17:52:46 -0400

    Added the new types
    -mhk225; mhk225@cornell.edu
    -Added new types
    -Implemented them in other functions in store
    - Made functions shorter

--- lib/store.ml
+++ lib/store.ml
@@ -22,6 +22,18 @@ type t = {
   eggs_price : float;
   milk_price : float;
   water_price : float;
+  clover_price : float;
+  rice_price : float;
+  lettuce_price : float;
+  mango_price : float;
+  bell_pepper_price : float;
+  butter_price : float;
+  sugar_price : float;
+  chocolate_price : float;
+  plant_food_price : float;
+  ladybug_price : float;
+  beef_price : float;
+  chicken_price : float;
 }
 
 type prices = {
@@ -29,58 +41,70 @@ type prices = {
   eggs_price : float;
   milk_price : float;
   water_price : float;
+  butter_price : float;
+  sugar_price : float;
+  chocolate_price : float;
+  plant_food_price : float;
+  ladybug_price : float;
+  beef_price : float;
+  chicken_price : float;
 }
 
-let create_store =
+let apply_discount price =
   let rand_val = Random.float 1.0 in
-  let usual =
-    {
-      cheese_price = 5.0;
-      eggs_price = 3.0;
-      milk_price = 5.0;
-      water_price = 1.0;
-    }
+  if rand_val < 0.5 then price else price *. 0.5
+
+let usual =
+  {
+    cheese_price = 5.0;
+    eggs_price = 3.0;
+    milk_price = 5.0;
+    water_price = 1.0;
+    butter_price = 2.0;
+    sugar_price = 1.5;
+    chocolate_price = 3.20;
+    plant_food_price = 2.0;
+    ladybug_price = 8.0;
+    beef_price = 6.0;
+    chicken_price = 5.50;
+  }
+
+let create_store =
+  let create_plant_and_get_price name =
+    Plant.get_price (Plant.apply_discount (Plant.create_plant name ""))
   in
   {
-    daisy_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Daisy" ""));
-    strawberry_price =
-      Plant.get_price
-        (Plant.apply_discount (Plant.create_plant "Strawberry" ""));
-    sunflower_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Sunflower" ""));
-    rose_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Rose" ""));
-    tulip_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Tulip" ""));
-    tomato_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Tomato" ""));
-    lemon_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Lemon" ""));
-    pineapple_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Pineapple" ""));
-    onion_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Onion" ""));
-    potato_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Potato" ""));
-    wheat_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Wheat" ""));
-    apple_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Apple" ""));
-    corn_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Corn" ""));
-    peach_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Peach" ""));
-    cactus_price =
-      Plant.get_price (Plant.apply_discount (Plant.create_plant "Cactus" ""));
-    cheese_price =
-      (if rand_val < 0.5 then usual.cheese_price else usual.cheese_price *. 0.5);
-    eggs_price =
-      (if rand_val < 0.5 then usual.eggs_price else usual.eggs_price *. 0.5);
-    milk_price =
-      (if rand_val < 0.5 then usual.milk_price else usual.milk_price *. 0.5);
-    water_price =
-      (if rand_val < 0.5 then usual.water_price else usual.water_price *. 0.5);
+    daisy_price = create_plant_and_get_price "Daisy";
+    strawberry_price = create_plant_and_get_price "Strawberry";
+    sunflower_price = create_plant_and_get_price "Sunflower";
+    rose_price = create_plant_and_get_price "Rose";
+    tulip_price = create_plant_and_get_price "Tulip";
+    tomato_price = create_plant_and_get_price "Tomato";
+    lemon_price = create_plant_and_get_price "Lemon";
+    pineapple_price = create_plant_and_get_price "Pineapple";
+    onion_price = create_plant_and_get_price "Onion";
+    potato_price = create_plant_and_get_price "Potato";
+    wheat_price = create_plant_and_get_price "Wheat";
+    apple_price = create_plant_and_get_price "Apple";
+    corn_price = create_plant_and_get_price "Corn";
+    peach_price = create_plant_and_get_price "Peach";
+    cactus_price = create_plant_and_get_price "Cactus";
+    clover_price = create_plant_and_get_price "Clover";
+    rice_price = create_plant_and_get_price "Rice";
+    lettuce_price = create_plant_and_get_price "Lettuce";
+    mango_price = create_plant_and_get_price "Mango";
+    cheese_price = apply_discount usual.cheese_price;
+    eggs_price = apply_discount usual.eggs_price;
+    milk_price = apply_discount usual.milk_price;
+    water_price = apply_discount usual.water_price;
+    butter_price = apply_discount usual.butter_price;
+    sugar_price = apply_discount usual.sugar_price;
+    chocolate_price = apply_discount usual.chocolate_price;
+    plant_food_price = apply_discount usual.plant_food_price;
+    ladybug_price = apply_discount usual.ladybug_price;
+    beef_price = apply_discount usual.beef_price;
+    chicken_price = apply_discount usual.chicken_price;
+    bell_pepper_price = create_plant_and_get_price "Bell Pepper";
   }
 
 let discount_plant price plant_name =
@@ -89,97 +113,65 @@ let discount_plant price plant_name =
   else ""
 
 let discount_general price name =
-  let usual =
-    {
-      cheese_price = 5.0;
-      eggs_price = 3.0;
-      milk_price = 5.0;
-      water_price = 1.0;
-    }
-  in
+  (* let usual = { cheese_price = 5.0; eggs_price = 3.0; milk_price = 5.0;
+     water_price = 1.0; butter_price = 2.0; sugar_price = 1.5; chocolate_price =
+     3.20; plant_food_price = 2.0; ladybug_price = 8.0; beef_price = 6.0;
+     chicken_price = 5.50; } in *)
   match name with
-  | "Cheese" -> if price < usual.cheese_price then "[DISCOUNT!!]" else ""
-  | "Eggs" -> if price < usual.eggs_price then "[DISCOUNT!!]" else ""
-  | "Milk" -> if price < usual.milk_price then "[DISCOUNT!!]" else ""
+  | "Cheese" -> if price < usual.cheese_price then "[DISCOUNT] 💸" else ""
+  | "Eggs" -> if price < usual.eggs_price then "[DISCOUNT] 💸" else ""
+  | "Milk" -> if price < usual.milk_price then "[DISCOUNT] 💸" else ""
+  | "Water" -> if price < usual.water_price then "[DISCOUNT] 💸" else ""
+  | "Butter" -> if price < usual.butter_price then "[[DISCOUNT] 💸" else ""
+  | "Sugar" -> if price < usual.sugar_price then "[DISCOUNT] 💸" else ""
+  | "Chocolate" -> if price < usual.chocolate_price then "[DISCOUNT] 💸]" else ""
+  | "Plant Food" ->
+      if price < usual.plant_food_price then "[DISCOUNT] 💸" else ""
+  | "Lady Bug" -> if price < usual.ladybug_price then "[DISCOUNT] 💸" else ""
+  | "Beef" -> if price < usual.beef_price then "[DISCOUNT] 💸" else ""
+  | "Chicken" -> if price < usual.chicken_price then "[DISCOUNT] 💸" else ""
   | _ -> ""
 
+let general_discount name price =
+  "\n" ^ name ^ ": ($" ^ string_of_float price ^ ") "
+  ^ discount_general price name
+
+let plant_and_price name price =
+  "\n" ^ name ^ ": ($" ^ string_of_float price ^ ") "
+  ^ discount_plant price name
+
 let print_store store =
-  "\n" ^ "Daisy: ($"
-  ^ string_of_float store.daisy_price
-  ^ ") "
-  ^ discount_plant store.daisy_price "Daisy"
-  ^ "\n" ^ "Sunflower: ($"
-  ^ string_of_float store.sunflower_price
-  ^ ") "
-  ^ discount_plant store.sunflower_price "Sunflower"
-  ^ "\n" ^ "Rose: ($"
-  ^ string_of_float store.rose_price
-  ^ ") "
-  ^ discount_plant store.rose_price "Rose"
-  ^ "\n" ^ "Tulip: ($"
-  ^ string_of_float store.tulip_price
-  ^ ") "
-  ^ discount_plant store.tulip_price "Tulip"
-  ^ "\n" ^ "Tomato: ($"
-  ^ string_of_float store.tomato_price
-  ^ ") "
-  ^ discount_plant store.tomato_price "Tomato"
-  ^ "\n" ^ "Corn: ($"
-  ^ string_of_float store.corn_price
-  ^ ") "
-  ^ discount_plant store.corn_price "Corn"
-  ^ "\n" ^ "Lemon: ($"
-  ^ string_of_float store.lemon_price
-  ^ ") "
-  ^ discount_plant store.lemon_price "Lemon"
-  ^ "\n" ^ "Onion: ($"
-  ^ string_of_float store.onion_price
-  ^ ") "
-  ^ discount_plant store.onion_price "Onion"
-  ^ "\n" ^ "Potato: ($"
-  ^ string_of_float store.potato_price
-  ^ ") "
-  ^ discount_plant store.potato_price "Potato"
-  ^ "\n" ^ "Wheat: ($"
-  ^ string_of_float store.wheat_price
-  ^ ") "
-  ^ discount_plant store.wheat_price "Wheat"
-  ^ "\n" ^ "Apple: ($"
-  ^ string_of_float store.apple_price
-  ^ ") "
-  ^ discount_plant store.apple_price "Apple"
-  ^ "\n" ^ "Peach: ($"
-  ^ string_of_float store.peach_price
-  ^ ") "
-  ^ discount_plant store.peach_price "Peach"
-  ^ "\n" ^ "Orange: ($"
-  ^ string_of_float store.pineapple_price
-  ^ ") "
-  ^ discount_plant store.pineapple_price "Pineapple"
-  ^ "\n" ^ "Strawberry: ($"
-  ^ string_of_float store.strawberry_price
-  ^ ") "
-  ^ discount_plant store.strawberry_price "Strawberry"
-  ^ "\n" ^ "Cactus: ($"
-  ^ string_of_float store.cactus_price
-  ^ ") "
-  ^ discount_plant store.cactus_price "Cactus"
-  ^ "\n" ^ "Cheese: ($"
-  ^ string_of_float store.cheese_price
-  ^ ") "
-  ^ discount_general store.cheese_price "Cheese"
-  ^ "\n" ^ "Water: ($"
-  ^ string_of_float store.water_price
-  ^ ") "
-  ^ discount_general store.water_price "Water"
-  ^ "\n" ^ "Eggs: ($"
-  ^ string_of_float store.eggs_price
-  ^ ") "
-  ^ discount_general store.eggs_price "Eggs"
-  ^ "\n" ^ "Cheese: ($"
-  ^ string_of_float store.cheese_price
-  ^ ") "
-  ^ discount_general store.milk_price "Milk"
+  plant_and_price "Daisy" store.daisy_price
+  ^ plant_and_price "Strawberry" store.strawberry_price
+  ^ plant_and_price "Sunflower" store.sunflower_price
+  ^ plant_and_price "Rose" store.rose_price
+  ^ plant_and_price "Tulip" store.tulip_price
+  ^ plant_and_price "Tomato" store.tomato_price
+  ^ plant_and_price "Lemon" store.lemon_price
+  ^ plant_and_price "Potato" store.potato_price
+  ^ plant_and_price "Onion" store.onion_price
+  ^ plant_and_price "Wheat" store.wheat_price
+  ^ plant_and_price "Apple" store.apple_price
+  ^ plant_and_price "Corn" store.corn_price
+  ^ plant_and_price "Peach" store.peach_price
+  ^ plant_and_price "Pineapple" store.pineapple_price
+  ^ plant_and_price "Cactus" store.cactus_price
+  ^ general_discount "Cheese" store.cheese_price
+  ^ general_discount "Eggs" store.eggs_price
+  ^ general_discount "Milk" store.milk_price
+  ^ general_discount "Water" store.water_price
+  ^ general_discount "Clover" store.clover_price
+  ^ general_discount "Rice" store.rice_price
+  ^ general_discount "Lettuce" store.lettuce_price
+  ^ general_discount "Mango" store.mango_price
+  ^ general_discount "Butter" store.butter_price
+  ^ general_discount "Sugar" store.sugar_price
+  ^ general_discount "Chocolate" store.chocolate_price
+  ^ general_discount "Plant Food" store.plant_food_price
+  ^ general_discount "Ladybug" store.ladybug_price
+  ^ general_discount "Beef" store.beef_price
+  ^ general_discount "Chicken" store.chicken_price
+  ^ general_discount "Bell Pepper" store.bell_pepper_price
 
 (* add seedling -> goes into garden *)
 (* add other item -> goes into inventory *)
@@ -188,107 +180,58 @@ let print_store store =
 (* let buy_item name = Inventory. *)
 
 let buy_item item_name store inv garden =
-  match item_name with
-  | "Daisy" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.daisy_price) garden) )
-  | "Sunflower" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.sunflower_price) garden) )
-  | "Rose" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.rose_price) garden) )
-  | "Tulip" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.tulip_price) garden) )
-  | "Tomato" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.tomato_price) garden) )
-  | "Corn" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.corn_price) garden) )
-  | "Lemon" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.lemon_price) garden) )
-  | "Onion" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.onion_price) garden) )
-  | "Potato" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.potato_price) garden) )
-  | "Wheat" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.wheat_price) garden) )
-  | "Apple" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.apple_price) garden) )
-  | "Pineapple" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.pineapple_price) garden) )
-  | "Peach" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.peach_price) garden) )
-  | "Strawberry" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.strawberry_price) garden) )
-  | "Cactus" ->
-      let () = print_endline "Name of Plant? (i.e. Benjamin)" in
-      let name = read_line () in
-      ( inv,
-        Garden.add_plant item_name name
-          (Garden.inc_money_amt (-1.0 *. store.cactus_price) garden) )
-  | "Eggs" ->
-      ( Inventory.add item_name inv,
-        Garden.inc_money_amt (-1.0 *. store.eggs_price) garden )
-  | "Milk" ->
-      ( Inventory.add item_name inv,
-        Garden.inc_money_amt (-1.0 *. store.milk_price) garden )
-  | "Cheese" ->
-      ( Inventory.add item_name inv,
-        Garden.inc_money_amt (-1.0 *. store.cheese_price) garden )
-  | "Water" ->
-      ( Inventory.add item_name inv,
-        Garden.inc_money_amt (-1.0 *. store.water_price) garden )
-  | _ -> (Inventory.add item_name inv, garden)
+  let price =
+    match item_name with
+    | "Daisy" -> store.daisy_price
+    | "Strawberry" -> store.strawberry_price
+    | "Sunflower" -> store.sunflower_price
+    | "Rose" -> store.rose_price
+    | "Tulip" -> store.tulip_price
+    | "Tomato" -> store.tomato_price
+    | "Lemon" -> store.lemon_price
+    | "Pineapple" -> store.pineapple_price
+    | "Onion" -> store.onion_price
+    | "Potato" -> store.potato_price
+    | "Wheat" -> store.wheat_price
+    | "Apple" -> store.apple_price
+    | "Corn" -> store.corn_price
+    | "Peach" -> store.peach_price
+    | "Cactus" -> store.cactus_price
+    | "Clover" -> store.clover_price
+    | "Rice" -> store.rice_price
+    | "Lettuce" -> store.lettuce_price
+    | "Mango" -> store.mango_price
+    | "Cheese" -> store.cheese_price
+    | "Eggs" -> store.eggs_price
+    | "Milk" -> store.milk_price
+    | "Water" -> store.water_price
+    | "Butter" -> store.butter_price
+    | "Chicken" -> store.chicken_price
+    | "Sugar" -> store.sugar_price
+    | "Chocolate" -> store.chocolate_price
+    | "Plant Food" -> store.plant_food_price
+    | "Lady Bug" -> store.ladybug_price
+    | "Beef" -> store.beef_price
+    | "Bell Pepper" -> store.bell_pepper_price
+    | _ -> 0.0 (* Default price for unknown items *)
+  in
+  let new_inv, new_money =
+    match item_name with
+    | "Milk"
+    | "Eggs"
+    | "Water"
+    | "Cheese"
+    | "Butter"
+    | "Sugar"
+    | "Chocolate"
+    | "Plant Food"
+    | "Lady Bug"
+    | "Beef" -> (Inventory.add item_name inv, garden)
+    | _ ->
+        let () = print_endline "Name of Plant? (i.e. Benjamin)" in
+        let name = read_line () in
+        let new_inv = Inventory.add item_name inv in
+        let new_money = Garden.inc_money_amt (-1.0 *. price) garden in
+        (new_inv, Garden.add_plant item_name name new_money)
+  in
+  (new_inv, new_money)
