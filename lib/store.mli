8fd232ac8d73c038351bd246009da95d8c5606fa
Mishita Kaja <mhk225@cornell.edu>
2024-05-06 18:22:24 -0400

    Wrote Specs for Store
    - mhk225; mhk225@cornell.edu
    - Implemented specifications for store

--- lib/store.mli
+++ lib/store.mli
@@ -1,7 +1,16 @@
-(* type t *)
 type t
+(** [t] represents a store. AF: Each store has items. RI: N/A*)
 
 val create_store : t
+(** [create_store] creates a store with the pricces of all the items *)
+
 val print_store : t -> string -> string
+(** [print_store s cat] prints the items of the store [s] using a specific
+    category the user enters [cat] *)
+
 val buy_item : string -> t -> Inventory.t -> Garden.t -> Inventory.t * Garden.t
+(** [buy_item item s inv g] adds what item the user wants into the inventory or
+    the store *)
+
 val has_item : string -> bool
+(** [has_item item] checks if the item that user enters is in the store *)
